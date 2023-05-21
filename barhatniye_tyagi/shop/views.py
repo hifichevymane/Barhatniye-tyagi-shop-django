2eed9bf7034e774060a4a8a38f1a4a7b8a2c5aa5
Maxim <blekbjek@gmail.com>
2023-05-21 17:50:14 +0300

    Add category sorting. Add README.md

--- barhatniye_tyagi/shop/views.py
+++ barhatniye_tyagi/shop/views.py
@@ -1,5 +1,6 @@
-from .models import Product
+from .models import Product, Category
 from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -15,10 +16,27 @@ class ProductsListView(ListView):
     context_object_name = 'products_list'
 
 
+# Products filter by category_slug
 class ProductsCategoryListView(ListView):
     template_name = 'shop/products_category_list.html'
     model = Product
     context_object_name = 'products_list'
+
+    # Get products with category = category_slug
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        category = get_object_or_404(Category, slug=category_slug)
+        queryset = super().get_queryset().filter(category=category)
+
+        return queryset
+    
+    # Get context data for html document and add category variable
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = self.kwargs['category_slug']
+        context['category'] = get_object_or_404(Category, slug=category_slug)
+
+        return context
 
 
 # Страница карточки товара
