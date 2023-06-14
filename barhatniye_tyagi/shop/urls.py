38ed277185a51e204274ad8c1fe9423e61dff6a2
Maxim <blekbjek@gmail.com>
2023-06-14 11:29:51 +0300

    Add new features

--- barhatniye_tyagi/shop/urls.py
+++ barhatniye_tyagi/shop/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('products/', views.ProductsListView.as_view(), name='products'),
     path('products/<slug:category_slug>', views.ProductsCategoryListView.as_view(), name='products-category'),
-    path('products/card/<int:pk>', views.CardDetailView.as_view(), name='card'),
+    path('products/card/<slug:slug>', views.CardDetailView.as_view(), name='card'),
     path('about/', views.AboutTemplateView.as_view(), name='about'),
 ]
